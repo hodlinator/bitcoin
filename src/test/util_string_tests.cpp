2fb2e72f45a1c81d036515861713371fec2bbe2c
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2024-09-19 22:18:50 +0200

    test: Prove+document ConstevalFormatString/tinyformat parity
    Co-Authored-By: l0rinc <pap.lorinc@gmail.com>
    Co-Authored-By: MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>

--- src/test/util_string_tests.cpp
+++ src/test/util_string_tests.cpp
@@ -10,13 +10,34 @@ using namespace util;
 
 BOOST_AUTO_TEST_SUITE(util_string_tests)
 
+template <typename... Tt>
+void TfmF(const char* fmt, const std::tuple<Tt...>& t)
+{
+    std::apply([fmt](const Tt&... ta){
+        tfm::format(fmt, ta...);
+    }, t);
+}
+
 // Helper to allow compile-time sanity checks while providing the number of
 // args directly. Normally PassFmt<sizeof...(Args)> would be used.
-template <unsigned NumArgs>
-inline void PassFmt(ConstevalFormatString<NumArgs> fmt)
+template <typename... Args>
+inline void PassFmt(ConstevalFormatString<sizeof...(Args)> fmt, const Args&... args)
 {
-    // This was already executed at compile-time, but is executed again at run-time to avoid -Wunused.
-    decltype(fmt)::Detail_CheckNumFormatSpecifiers(fmt.fmt);
+    ConstevalFormatString<sizeof...(Args)>::Detail_CheckNumFormatSpecifiers(fmt.fmt);
+
+    // Prove parity with tinyformat
+    tfm::format(fmt.fmt, args...);
+    if constexpr (sizeof...(Args) > 0) {
+        BOOST_CHECK_THROW(TfmF(fmt.fmt, std::tuple_cat(std::array<int, sizeof...(Args) - 1>{})), tfm::format_error);
+    }
+}
+template <unsigned WrongNumArgs, unsigned CorrectArgs>
+inline void PassFmtIncorrect(ConstevalFormatString<WrongNumArgs> fmt)
+{
+    // Disprove parity with tinyformat
+    static_assert(WrongNumArgs != CorrectArgs);
+    TfmF(fmt.fmt, std::tuple_cat(std::array<int, CorrectArgs>{}));
+    BOOST_CHECK_THROW(TfmF(fmt.fmt, std::tuple_cat(std::array<int, WrongNumArgs>{})), tfm::format_error);
 }
 template <unsigned WrongNumArgs>
 inline void FailFmtWithError(std::string_view wrong_fmt, std::string_view error)
@@ -28,39 +49,39 @@ inline void FailFmtWithError(std::string_view wrong_fmt, std::string_view error)
 
 BOOST_AUTO_TEST_CASE(ConstevalFormatString_NumSpec)
 {
-    PassFmt<0>("");
-    PassFmt<0>("%%");
-    PassFmt<1>("%s");
-    PassFmt<0>("%%s");
-    PassFmt<0>("s%%");
-    PassFmt<1>("%%%s");
-    PassFmt<1>("%s%%");
-    PassFmt<0>(" 1$s");
-    PassFmt<1>("%1$s");
-    PassFmt<1>("%1$s%1$s");
-    PassFmt<2>("%2$s");
-    PassFmt<2>("%2$s 4$s %2$s");
-    PassFmt<129>("%129$s 999$s %2$s");
-    PassFmt<1>("%02d");
-    PassFmt<1>("%+2s");
-    PassFmt<1>("%.6i");
-    PassFmt<1>("%5.2f");
-    PassFmt<1>("%#x");
-    PassFmt<1>("%1$5i");
-    PassFmt<1>("%1$-5i");
-    PassFmt<1>("%1$.5i");
+    PassFmt("");
+    PassFmt("%%");
+    PassFmt("%s", "foo");
+    PassFmt("%%s");
+    PassFmt("s%%");
+    PassFmt("%%%s", "foo");
+    PassFmt("%s%%", "foo");
+    PassFmt(" 1$s");
+    PassFmt("%1$s", "foo");
+    PassFmt("%1$s%1$s", "foo");
+    PassFmt("%2$s", "foo", "bar");
+    PassFmt("%2$s 4$s %2$s", "foo", "bar");
+    PassFmt("%12$s 999$s %2$s", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+    PassFmt("%02d", 1);
+    PassFmt("%+2s", "foo");
+    PassFmt("%.6i", 1);
+    PassFmt("%5.2f", 0.4f);
+    PassFmt("%#x", 1);
+    PassFmt("%1$5i", 1);
+    PassFmt("%1$-5i", 1);
+    PassFmt("%1$.5i", 12);
     // tinyformat accepts almost any "type" spec, even '%', or '_', or '\n'.
-    PassFmt<1>("%123%");
-    PassFmt<1>("%123%s");
-    PassFmt<1>("%_");
-    PassFmt<1>("%\n");
+    PassFmt("%123%", 1);
+    PassFmt("%123%s", 1);
+    PassFmt("%_", 1);
+    PassFmt("%\n", 1);
 
     // The `*` specifier behavior is unsupported and can lead to runtime
     // errors when used in a ConstevalFormatString. Please refer to the
     // note in the ConstevalFormatString docs.
-    PassFmt<1>("%*c");
-    PassFmt<2>("%2$*3$d");
-    PassFmt<1>("%.*f");
+    PassFmtIncorrect<1, 2>("%*c");
+    PassFmtIncorrect<2, 3>("%2$*3$d");
+    PassFmtIncorrect<1, 2>("%.*f");
 
     auto err_mix{"Format specifiers must be all positional or all non-positional!"};
     FailFmtWithError<1>("%s%1$s", err_mix);
