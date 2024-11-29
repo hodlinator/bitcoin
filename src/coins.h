209c1220b5594bbaf22aa56f6b68f781cda1269f
Lőrinc <pap.lorinc@gmail.com>
2024-11-29 14:10:32 +0100

    coins, refactor: Split up AddFlags to remove invalid states
    CCoinsCacheEntry provided general access to its internal flags state, even though in reality it could only be clean, fresh, dirty or fresh|dirty.
    
    After it got dirtied we couldn't set the state back to clean by AddFlags(0) - tests were explicitly checking against that.
    
    This commit cleans up the public interface to make this distinction cleaner and invalid behavior impossible instead of just checked by tests.
    
    Also modernized `EmplaceCoinInternalDANGER` since it was already modified.
    
    Also makes AddFlags static.
    
    Co-authored-by: Andrew Toth <andrewstoth@gmail.com>

--- src/coins.h
+++ src/coins.h
@@ -110,7 +110,7 @@ struct CCoinsCacheEntry
 private:
     /**
      * These are used to create a doubly linked list of flagged entries.
-     * They are set in AddFlags and unset in ClearFlags.
+     * They are set in SetDirty, SetFresh, and unset in SetClean.
      * A flagged entry is any entry that is either DIRTY, FRESH, or both.
      *
      * DIRTY entries are tracked so that only modified entries can be passed to
@@ -156,24 +156,25 @@ public:
     explicit CCoinsCacheEntry(Coin&& coin_) noexcept : coin(std::move(coin_)) {}
     ~CCoinsCacheEntry()
     {
-        ClearFlags();
+        SetClean();
     }
 
-    //! Adding a flag also requires a self reference to the pair that contains
-    //! this entry in the CCoinsCache map and a reference to the sentinel of the
-    //! flagged pair linked list.
-    inline void AddFlags(uint8_t flags, CoinsCachePair& self, CoinsCachePair& sentinel) noexcept
+    //! Adding a flag requires a reference to the sentinel of the flagged pair linked list.
+    static inline void AddFlags(uint8_t flags, CoinsCachePair& pair, CoinsCachePair& sentinel) noexcept
     {
-        Assume(&self.second == this);
-        if (!m_flags && flags) {
-            m_prev = sentinel.second.m_prev;
-            m_next = &sentinel;
-            sentinel.second.m_prev = &self;
-            m_prev->second.m_next = &self;
+        Assume(flags & (DIRTY | FRESH));
+        if (!pair.second.m_flags) {
+            pair.second.m_prev = sentinel.second.m_prev;
+            pair.second.m_next = &sentinel;
+            sentinel.second.m_prev = &pair;
+            pair.second.m_prev->second.m_next = &pair;
         }
-        m_flags |= flags;
+        pair.second.m_flags |= flags;
     }
-    inline void ClearFlags() noexcept
+    static inline void SetDirty(CoinsCachePair& pair, CoinsCachePair& sentinel) noexcept { AddFlags(DIRTY, pair, sentinel); }
+    static inline void SetFresh(CoinsCachePair& pair, CoinsCachePair& sentinel) noexcept { AddFlags(FRESH, pair, sentinel); }
+
+    inline void SetClean() noexcept
     {
         if (!m_flags) return;
         m_next->second.m_prev = m_prev;
@@ -279,13 +280,13 @@ struct CoinsViewCacheCursor
     {
         const auto next_entry{current.second.Next()};
         // If we are not going to erase the cache, we must still erase spent entries.
-        // Otherwise clear the flags on the entry.
+        // Otherwise, clear the state of the entry.
         if (!m_will_erase) {
             if (current.second.coin.IsSpent()) {
                 m_usage -= current.second.coin.DynamicMemoryUsage();
                 m_map.erase(current.first);
             } else {
-                current.second.ClearFlags();
+                current.second.SetClean();
             }
         }
         return next_entry;
