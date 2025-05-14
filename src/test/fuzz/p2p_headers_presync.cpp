ed313d166bfb681da538f9fe3af3ca41423bdf76
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2025-05-14 15:08:04 +0200

    doc: Fix codespell warnings
    Regarding "re-used"/"re-using", see: https://github.com/bitcoin/bitcoin/pull/30700#discussion_r1730112466
    
    "anc" is commonly used for "ancestor".
    
    Ignored cases:
    src/cluster_linearize.h:961: anc ==> and
    src/cluster_linearize.h:962: anc ==> and
    src/cluster_linearize.h:963: anc ==> and
    src/node/mini_miner.cpp:205: anc ==> and
    src/node/mini_miner.cpp:221: anc ==> and
    src/node/mini_miner.cpp:227: anc ==> and
    src/node/mini_miner.cpp:230: anc ==> and
    src/test/fuzz/cluster_linearize.cpp:724: anc ==> and
    src/test/fuzz/cluster_linearize.cpp:725: anc ==> and
    src/test/fuzz/p2p_headers_presync.cpp:35: re-used ==> reused
    src/test/fuzz/p2p_headers_presync.cpp:35: re-used ==> reused
    src/test/fuzz/policy_estimator_io.cpp:30: Re-using ==> Reusing
    src/test/fuzz/txgraph.cpp:943: anc ==> and
    src/test/fuzz/txgraph.cpp:944: anc ==> and
    src/test/fuzz/txgraph.cpp:945: anc ==> and

--- src/test/fuzz/p2p_headers_presync.cpp
+++ src/test/fuzz/p2p_headers_presync.cpp
@@ -34,7 +34,7 @@ public:
         peerman_opts.max_headers_result = FUZZ_MAX_HEADERS_RESULTS;
         // The peerman's rng is a global that is re-used, so it will be re-used
         // and may cause non-determinism between runs. This may even influence
-        // the global RNG, because seeding may be done from the gloabl one. For
+        // the global RNG, because seeding may be done from the global one. For
         // now, avoid it influencing the global RNG, and initialize it with a
         // constant instead.
         peerman_opts.deterministic_rng = true;
