c2088a74586742cadc0041cab8782c423eda3bd0
Roman Zeyde <me@romanzey.de>
2025-09-27 09:15:17 +0200

    rest: allow reading partial block data from storage
    It will allow fetching specific transactions using an external index,
    following https://github.com/bitcoin/bitcoin/pull/32541#issuecomment-3267485313.
    
    Co-authored-by: Hodlinator <172445034+hodlinator@users.noreply.github.com>

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -1046,6 +1046,11 @@ bool BlockManager::ReadBlock(CBlock& block, const CBlockIndex& index) const
 
 bool BlockManager::ReadRawBlock(std::vector<std::byte>& block, const FlatFilePos& pos) const
 {
+    return ReadRawBlockPart(block, pos, 0, std::nullopt);
+}
+
+bool BlockManager::ReadRawBlockPart(std::vector<std::byte>& data, const FlatFilePos& pos, size_t part_offset, std::optional<size_t> part_size) const
+{
     if (pos.nPos < STORAGE_HEADER_BYTES) {
         // If nPos is less than STORAGE_HEADER_BYTES, we can't read the header that precedes the block data
         // This would cause an unsigned integer underflow when trying to position the file cursor
@@ -1077,8 +1082,24 @@ bool BlockManager::ReadRawBlock(std::vector<std::byte>& block, const FlatFilePos
             return false;
         }
 
-        block.resize(blk_size); // Zeroing of memory is intentional here
-        filein.read(block);
+        if (part_offset > blk_size) {
+            LogError("Read from block file failed: invalid offset: %u", part_offset);
+            return false;
+        }
+
+        size_t size = blk_size - part_offset;
+        if (part_size.has_value()) {
+            if (*part_size > size || *part_size == 0) {
+                LogError("Read from block file failed: invalid size: %u", *part_size);
+                return false;
+            }
+            size = *part_size;
+        }
+
+
+        data.resize(size); // Zeroing of memory is intentional here
+        if (part_offset > 0) filein.seek(part_offset, SEEK_CUR);
+        filein.read(data);
     } catch (const std::exception& e) {
         LogError("Read from block file failed: %s for %s while reading raw block", e.what(), pos.ToString());
         return false;
