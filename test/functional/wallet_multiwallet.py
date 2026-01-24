c411e10e036fddf147db19f62e2e5bf41b49f1fb
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2026-01-24 13:54:08 +0100

    qa: Check for platform-independent part of error message
    On Windows one gets different error messages depending on whether running a native build or cross build.

--- test/functional/wallet_multiwallet.py
+++ test/functional/wallet_multiwallet.py
@@ -158,7 +158,7 @@ class MultiWalletTest(BitcoinTestFramework):
 
         # should raise rpc error if wallet path can't be created
         err_code = -4
-        assert_raises_rpc_error(err_code, "filesystem error:" if platform.system() != 'Windows' else "create_directories:", self.nodes[0].createwallet, "w8/bad")
+        assert_raises_rpc_error(err_code, "Wallet file verification failed. ", self.nodes[0].createwallet, "w8/bad")
 
         # check that all requested wallets were created
         self.stop_node(0)
