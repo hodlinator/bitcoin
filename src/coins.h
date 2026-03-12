6be33a87073599d7109716ff785c6b6c38aeb7ae
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2026-03-12 23:22:04 +0100

    coins: filter inputs spending outputs of same block in ProcessInput
    This is a performance improvement, because we can skip checking on disk
    that the input does not exist.
    
    Co-authored-by: l0rinc <pap.lorinc@gmail.com>

--- src/coins.h
+++ src/coins.h
@@ -24,6 +24,7 @@
 #include <cassert>
 #include <cstdint>
 
+#include <algorithm>
 #include <atomic>
 #include <functional>
 #include <optional>
@@ -585,6 +586,17 @@ private:
     QuickHashHasher m_hasher;
 
     /**
+     * The sorted quick hash of txids of all txs in the block being fetched. This is used to filter out inputs that
+     * are created earlier in the same block, since they will not be in the db or the cache.
+     * Using an 8 byte quick hash is a performance improvement, versus storing the entire 32 bytes. In case of a
+     * collision of an input being spent having the same quick hash as a txid of a tx elsewhere in the block,
+     * the input will not be fetched in the background. The input will still be fetched later on the main thread.
+     * Using a sorted vector and binary search lookups is a performance improvement. It is faster than
+     * using std::unordered_set with salted hash or std::set.
+     */
+    std::vector<uint64_t> m_txids{};
+
+    /**
      * Claim and fetch the next input in the queue.
      *
      * @return true if there are more inputs in the queue to fetch
@@ -596,6 +608,11 @@ private:
         if (i >= m_inputs.size()) [[unlikely]] return false;
 
         auto& input{m_inputs[i]};
+        // Inputs spending a coin from a tx earlier in the block won't be in the cache or db
+        if (std::ranges::binary_search(m_txids, m_hasher(input.outpoint.hash))) {
+            return true;
+        }
+
         if (auto coin{base->PeekCoin(input.outpoint)}) [[likely]] input.coin.emplace(std::move(*coin));
         return i < m_inputs.size() - 1;
     }
@@ -605,6 +622,7 @@ private:
     {
         m_inputs.clear();
         m_input_head.store(0, std::memory_order_relaxed);
+        m_txids.clear();
     }
 
     std::optional<Coin> FetchCoinFromBase(const COutPoint& outpoint) const override
@@ -614,12 +632,13 @@ private:
             if (input.outpoint != outpoint) continue;
             // We can move the coin since we won't access this input again.
             if (input.coin) [[likely]] return std::move(*input.coin);
-            // Already spent
-            return std::nullopt;
+            // If we get here, then this block has missing or spent inputs or there is a txid quick hash collision.
+            break;
         }
 
-        assert(outpoint == COutPoint{}); // Only coinbase inputs should be missing.
-        return std::nullopt;
+        // We will only get here for BIP30 checks, txid quick hash collisions,
+        // a block with missing or spent inputs, or attempts to look up coinbase inputs.
+        return base->PeekCoin(outpoint);
     }
 
 protected:
@@ -636,10 +655,13 @@ public:
     //! Start fetching inputs from block.
     [[nodiscard]] ResetGuard StartFetching(const CBlock& block LIFETIMEBOUND) noexcept
     {
-        // Loop through the inputs of the block and set them in the queue.
+        // Loop through the inputs of the block and set them in the queue. Also construct the set of txids to filter.
         for (const auto& tx : block.vtx | std::views::drop(1)) [[likely]] {
             for (const auto& input : tx->vin) [[likely]] m_inputs.emplace_back(input.prevout);
+            m_txids.emplace_back(m_hasher(tx->GetHash()));
         }
+        // Sort txids so we can do binary search lookups.
+        std::ranges::sort(m_txids);
         while (ProcessInput()) [[likely]] {}
         return CreateResetGuard();
     }
