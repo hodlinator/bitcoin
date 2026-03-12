ad90aacea2ab6c573cecc42f5a4078db94a460a6
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2026-03-12 23:01:30 +0100

    coins: introduce thread pool in CoinsViewOverlay
    Prepares for ProcessInput to be called from multiple threads.
    
    Introduce a ThreadPool shared pointer to CoinsViewOverlay. A pool managed
    externally can be passed in the constructor, or if nullptr is passed the
    overlay will create and start its own pool.
    
    Co-authored-by: l0rinc <pap.lorinc@gmail.com>

--- src/coins.h
+++ src/coins.h
@@ -20,6 +20,7 @@
 #include <util/check.h>
 #include <util/overflow.h>
 #include <util/hasher.h>
+#include <util/threadpool.h>
 
 #include <cassert>
 #include <cstdint>
@@ -27,11 +28,14 @@
 #include <algorithm>
 #include <atomic>
 #include <functional>
+#include <memory>
 #include <optional>
 #include <ranges>
 #include <unordered_map>
 #include <utility>
 #include <vector>
+
+constexpr auto COINS_VIEW_OVERLAY_WORKER_THREADS{4};
 
 /**
  * A UTXO entry.
@@ -671,6 +675,8 @@ private:
         return base->PeekCoin(outpoint);
     }
 
+    std::shared_ptr<ThreadPool> m_thread_pool;
+
 protected:
     void Reset() noexcept override
     {
@@ -679,8 +685,17 @@ protected:
     }
 
 public:
-    explicit CoinsViewOverlay(CCoinsView* in_base, bool deterministic = false) noexcept
-        : CCoinsViewCache{in_base, deterministic}, m_hasher{deterministic} {}
+    explicit CoinsViewOverlay(CCoinsView* in_base, bool deterministic = false,
+                              std::shared_ptr<ThreadPool> thread_pool = nullptr) noexcept
+        : CCoinsViewCache{in_base, deterministic},
+          m_hasher{deterministic},
+          m_thread_pool{std::move(thread_pool)}
+    {
+        if (m_thread_pool == nullptr) {
+            m_thread_pool = std::make_shared<ThreadPool>("inputfetch");
+            m_thread_pool->Start(COINS_VIEW_OVERLAY_WORKER_THREADS);
+        }
+    }
 
     //! Start fetching inputs from block.
     [[nodiscard]] ResetGuard StartFetching(const CBlock& block LIFETIMEBOUND) noexcept
